8430905e2c18a9f36ee694b4455e8d395d8ae766
林㛄廷 <ting@linyitingdeMacBook-Air-2.local>
2026-04-25 19:18:43 +0800

    config: update supabase api credentials

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vtbrnavffykjmkdmiijt.supabase.co'
-const supabaseKey = 'sb_publishable_BeR9tEvAHHPfboo1p_T_eg_wIdG9GDZ'
+const supabaseUrl = 'https://xwbegziyfjoysjmrtsqy.supabase.co'
+const supabaseKey = 'sb_publishable_m9UVUpnNMuXh6VWxj2t6cQ_SVKQKaOH'
 
 export const supabaseClient = createClient(supabaseUrl, supabaseKey)
